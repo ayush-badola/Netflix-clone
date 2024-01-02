6e2fb9194e3ea1803f2a17c34b6476363d9bdeff
Ayush Badola <glitchy.56@outlook.com>
2024-01-03 00:21:35 +0530

    progressing with adding the required modules

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("index");
 });
+
+router.post();
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect("mongodb://127.0.0.1:27017/netcdata");
 
+
+const user_schema = mongoose.schema ({
+  name: String,
+  username: String,
+  email : String,
+  number: Number,
+  password: String,
+  profile: String
+})
+
+user_schema.plugin(plm);
 module.exports = router;
+module.exports = mongoose.model("user",user_schema);
